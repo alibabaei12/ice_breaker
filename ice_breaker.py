bee7c608a2a76fb16b2e1407db6f1e83ff6170b1
Ali Babaei <alibabaei@Alis-MacBook-Air.local>
2024-01-03 19:15:47 -0800

    final version

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,27 +1,39 @@
+from typing import Tuple
+
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parsers import person_intel_parser, PersonIntel
 from third_parties.linkedin import scrape_linkedin_profile
 
 
-if __name__ == "__main__":
-    linkedin_profile_url = linkedin_lookup_agent(name="rayna ji amazon")
+def ice_break(name: str) -> Tuple[PersonIntel, str]:
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
     summary_template = """
-        given the linkedin information {information} about the person from I want to create:
-        1. a short summary
-        2. two interesting facts about them
-    """
+            given the linkedin information {information} about the person from I want to create:
+            1. a short summary
+            2. two interesting facts about them
+                    \n{format_instructions}
+        """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url=linkedin_profile_url
-    )
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
+    result = chain.run(information=linkedin_data)
 
-    print(chain.run(information=linkedin_data))
+    print(result)
+    return person_intel_parser.parse(result), linkedin_data.get("profile_pic_url")
+
+
+if __name__ == "__main__":
+    ice_break(name="rayna ji")
